90c99510f764d5ca94ac993d1348a120c67ae007
Jamie Gardner <jamie@thatgardnerone.com>
2022-05-06 17:51:30 +0100

    Hardcode initial game objects

--- js/Universe.js
+++ js/Universe.js
@@ -6,6 +6,39 @@ class Universe {
     }
 
     init() {
-        console.log("Universe loaded", this)
+        // Draw the background
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image, 0, 0);
+        }
+        image.src = "./images/maps/DemoLower.png";
+
+        // Draw the player
+        const x = 1;
+        const y = 2;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0, 0,
+                32, 32,
+                x * 16 - 7, y * 16 - 17,
+                32, 32
+            );
+        }
+        shadow.src = "./images/characters/shadow.png";
+
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, 0,
+                32, 32,
+                x * 16 - 7, y * 16 - 18,
+                32, 32
+            );
+        }
+        hero.src = "./images/characters/people/hero.png";
     }
 }
